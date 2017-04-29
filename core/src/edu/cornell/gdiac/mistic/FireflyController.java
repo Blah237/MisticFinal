0f625294e0892682bd694bfb80f2aadc33037293
beau <ibm24@cornell.edu>
2017-04-29 11:56:32 -0400

    no more lantern bugs pleas

--- core/src/edu/cornell/gdiac/mistic/FireflyController.java
+++ core/src/edu/cornell/gdiac/mistic/FireflyController.java
@@ -29,7 +29,7 @@ public class FireflyController {
     private Vector2 scale;
     /**The time between firefly spawns*/
     private float SPAWN_TIME;
-    private int MAX_FIREFLIES=12;
+    private int MAX_FIREFLIES=10;
     private int maxfireflies = MAX_FIREFLIES;
     public Firefly[] fireflies;
     private FilmStrip animate;
